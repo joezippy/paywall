c5b820a98ea7fed936848cd494ea84b46cb4e30a
joezippy <kcolussi@gmail.com>
2017-12-04 20:20:35 -0700

    Added CSV report for better accounting integration
    This reduced the sheet "imports calls" google was not happy with the number external calls in the accounting system, this allows a single sheet import with all data.
    
    report.py/CSV=yes

--- report.py
+++ report.py
@@ -223,6 +223,92 @@ def do_html_output(candidates,src_file,details,debug):
       print("</pre></body></html>")
       return
 
+def do_csv_output(candidates,src_file,debug):
+      COINMARKET_DASH_PRICE = get_dash_price(debug)
+      CHAIN_RECEIVED_TOTAL_DASH = 0.0      
+      CHAIN_RECEIVED_TOTAL_US = 0.0
+      CHAIN_SENT_TOTAL_DASH = 0.0      
+      CHAIN_SENT_TOTAL_US = 0.0
+      PAYWALL_DEPOSIT_TOTAL_DASH = 0.0      
+      PAYWALL_DEPOSIT_TOTAL_US = 0.0
+      data_out = []
+      payee_keys = candidates.keys()
+      for key in payee_keys:
+            payee = candidates[key]
+
+            address_received_total_dash = float(payee["total_received"])
+            address_received_total_us = float(payee["total_received"]) * COINMARKET_DASH_PRICE
+            CHAIN_RECEIVED_TOTAL_DASH = CHAIN_RECEIVED_TOTAL_DASH + address_received_total_dash
+            CHAIN_RECEIVED_TOTAL_US = CHAIN_RECEIVED_TOTAL_US + address_received_total_us
+            
+            address_sent_total_dash = float(payee["total_sent"])
+            address_sent_total_us = float(payee["total_sent"]) * COINMARKET_DASH_PRICE
+            CHAIN_SENT_TOTAL_DASH = CHAIN_SENT_TOTAL_DASH + address_sent_total_dash
+            CHAIN_SENT_TOTAL_US = CHAIN_SENT_TOTAL_US + address_sent_total_us
+            
+            address_final_total_dash = float(payee["final_balance"])
+            address_final_total_us = float(payee["final_balance"]) * COINMARKET_DASH_PRICE
+
+            # loop and calc all paywment here
+            address_deposit_total_dash = 0.0
+            address_deposit_total_us = 0.0
+            for pay in payee["payments"] :
+                  if (debug): print(str(pay["amount"]) + " * "
+                                    + str(pay["dash_price"])
+                                    + " Dash => "
+                                    + str(pay["amount"]*pay["dash_price"]) + " USD")
+                  address_deposit_total_dash = address_deposit_total_dash + pay["amount"]
+                  address_deposit_total_us = address_deposit_total_us + pay["amount"]*pay["dash_price"]
+            PAYWALL_DEPOSIT_TOTAL_DASH = PAYWALL_DEPOSIT_TOTAL_DASH + address_deposit_total_dash
+            PAYWALL_DEPOSIT_TOTAL_US = PAYWALL_DEPOSIT_TOTAL_US + address_deposit_total_us
+
+            data_out.append(str(payee["address"]) + ","
+                            + str(format(round(address_received_total_dash,4), '.4f')) + ","
+                            + str(format(round(address_sent_total_dash,4), '.4f')) + ","
+                            + str(format(round(address_final_total_dash,4), '.4f')) + ","
+                            + str(format(round(address_deposit_total_dash,4), '.4f')) + ","
+                            + str(format(round(address_received_total_us,2), '.2f')) + ","
+                            + str(format(round(address_sent_total_us,2), '.2f')) + "," 
+                            + str(format(round(address_final_total_us,2), '.2f')) + ","
+                            + str(format(round(address_deposit_total_us,2), '.2f')) + ","
+                            + str(format(round(address_received_total_dash,4), '.4f') == format(round(address_deposit_total_dash,4), '.4f')))
+
+
+      data_out.append("")
+
+      PAYWALL_TOTAL_DASH = 0.0      
+      PAYWALL_TOTAL_US = 0.0
+      data_qtr_out = {}      
+      payee_keys = candidates.keys()
+      for key in payee_keys:
+            payee = candidates[key]
+            # loop and calc all paywment here
+            for pay in payee["payments"] :
+                  if (debug) : print(str(payee["address"]) + " -> " + str(pay["completed_quarter"])
+                        + " -> (" + str(pay["amount"]) + " * " + str(pay["dash_price"]) + ") Dash => " + str(pay["amount"]*pay["dash_price"]) + " USD")
+                  PAYWALL_TOTAL_DASH = PAYWALL_TOTAL_DASH + pay["amount"]
+                  PAYWALL_TOTAL_US = PAYWALL_TOTAL_US + pay["amount"]*pay["dash_price"]
+                  if (pay["completed_quarter"] in data_qtr_out) :
+                        qtr_dash = float(data_qtr_out[pay["completed_quarter"]]["Dash"])
+                        qtr_us = float(data_qtr_out[pay["completed_quarter"]]["USD"])
+                        data_qtr_out[pay["completed_quarter"]] = {"Dash" : str(format(round(pay["amount"]+qtr_dash,4), '.4f')) ,
+                                                              "USD" : str(format(round((pay["amount"]*pay["dash_price"])+qtr_us,2), '.2f'))}
+                  else:
+                        #first find of qtr
+                        data_qtr_out[pay["completed_quarter"]] = {"Dash" : str(format(round(pay["amount"],4), '.4f')) ,
+                                                              "USD" : str(format(round(pay["amount"]*pay["dash_price"],2), '.2f'))}
+                                    
+      print("Content-Type: text/csv\n\n")
+      print("address,received_total_dash,sent_total_dash,final_total_dash,deposit_paywall_total_dash,received_total_us,sent_total_us," 
+                            + "final_total_us,deposit_paywall_total_us,chain=paywall?")
+      for line in data_out:
+            print(line)
+
+      print("quarter,dash,usd")
+      for key in sorted(data_qtr_out):
+            print("'" + str(key) + "," + str(data_qtr_out[key]["Dash"]) + ","  + str(data_qtr_out[key]["USD"]))
+      return
+
 def report_output(json_directory, json_file, debug):
       json_dir = json_directory
       addr_filename = json_file
@@ -250,6 +336,12 @@ def report_output(json_directory, json_file, debug):
             details = False
       else:
             details = str2bool(form.getvalue("DETAILS"))
+
+      csv = False
+      if (form.getvalue("CSV") is None):
+            csv = False
+      else:
+            csv = str2bool(form.getvalue("CSV"))
             
       completed_quarter = "" if not form.getvalue("completed_quarter") else form.getvalue("completed_quarter")
       completed_quarter_curr = "" if not form.getvalue("completed_quarter_curr") else form.getvalue("completed_quarter_curr")
@@ -283,6 +375,8 @@ def report_output(json_directory, json_file, debug):
             do_wp_output(candidates,debug)
       elif (qtr_report):
             do_qtr_output(candidates,completed_quarter,completed_quarter_curr,src_file,debug)
+      elif (csv):
+            do_csv_output(candidates,src_file,debug)
       else:
             do_html_output(candidates,src_file,details,debug)
             
